0cda254fe7d0a9902c396400395388b8fb636a96
eco <erinconeal@outlook.com>
2016-09-27 16:31:28 -0600

    first commit

---
+++ js/mainCtrl.js
@@ -0,0 +1,18 @@
+angular.module('quoteBook').controller('mainCtrl', function($scope, mainService) {
+  $scope.quotes = mainService.getData();
+
+  $scope.deleteMe = function(textToDelete) {
+    mainService.removeData(textToDelete);
+  }
+
+  $scope.addQuote = function() {
+    var newQuote = {
+      text: $scope.newQuoteText,
+      author: $scope.newQuoteAuthor
+    }
+    if (mainService.addData(newQuote)) {
+      $scope.newQuoteText = '';
+      $scope.newQuoteAuthor = '';
+    }
+  }
+})
